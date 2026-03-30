bd5524c242806ea4751c2f598c99a4f1f626b012
JuD4Mo <juan_d.munoz_o@uao.edu.co>
2026-03-29 19:07:19 -0500

    feat: set up initial project structure with Docker, Prisma, and NATS integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AdministrativeDataModule } from './administrative-data/administrative-data.module';
 
 @Module({
   imports: [AdministrativeDataModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
