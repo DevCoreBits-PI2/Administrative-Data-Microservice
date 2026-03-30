c3a776e77c1f2f5ac465f271d58669f40c937bac
JuD4Mo <juan_d.munoz_o@uao.edu.co>
2026-03-30 01:17:58 -0500

    feat: update Dockerfile and package.json for NATS integration; enhance main.ts with microservice setup and validation

--- src/administrative-data/administrative-data.module.ts
+++ src/administrative-data/administrative-data.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AdministrativeDataService } from './administrative-data.service';
 import { AdministrativeDataController } from './administrative-data.controller';
+import { NatsModule } from 'src/transports/nats.module';
 
 @Module({
   controllers: [AdministrativeDataController],
   providers: [AdministrativeDataService],
+  imports:[
+    NatsModule
+  ]
 })
 export class AdministrativeDataModule {}
